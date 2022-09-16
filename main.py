d103b3db61947046e835bc3421bdc82ea345c364
Aleks Mamontov <mamontov1996@mail.ru>
2022-09-16 14:44:19 +1000

    add lambda in main.py

--- main.py
+++ main.py
@@ -34,6 +34,28 @@ def get_shop_list_by_person(dishes, person_count, cook_book):
     return json.dumps(shop_ing, indent=4, ensure_ascii=False)
 
 
+# def concat_file(file1, file2, file3):
+#     count_line = []
+#     for file in (file1, file2, file3):
+#         name = file
+#         with open(file, encoding='utf8') as files:
+#             content_file = files.readlines()
+#             count_line.append([name, content_file])
+#
+#     for i in range(len(count_line) - 1):
+#         if len(count_line[i][1]) > len(count_line[i+1][1]):
+#             count_line[i], count_line[i+1] = count_line[i+1], count_line[i]
+#
+#     with open('4.txt', 'w', encoding='utf8') as files:
+#         for file in count_line:
+#             res = f'{file[0]}\n{len(file[1])}\n{"".join(file[1])}\n'
+#             files.writelines(res)
+#
+#     with open('4.txt', 'r', encoding='utf8') as files:
+#         content = files.read()
+#         print(content)
+
+
 def concat_file(file1, file2, file3):
     count_line = []
     for file in (file1, file2, file3):
@@ -42,9 +64,7 @@ def concat_file(file1, file2, file3):
             content_file = files.readlines()
             count_line.append([name, content_file])
 
-    for i in range(len(count_line) - 1):
-        if len(count_line[i][1]) > len(count_line[i+1][1]):
-            count_line[i], count_line[i+1] = count_line[i+1], count_line[i]
+    count_line.sort(key=lambda x: len(x[1]))
 
     with open('4.txt', 'w', encoding='utf8') as files:
         for file in count_line:
